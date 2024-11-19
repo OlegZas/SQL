1afad961e3695264246b4871bd13c34348fd66e3
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-18 20:49:00 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+--1. 
 /*Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
 Return the result table in any order.
 The result format is in the following example.
