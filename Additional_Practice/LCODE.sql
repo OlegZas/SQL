b569d355b16827e2fd5edf669c33f1fd88be1ef0
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-18 20:20:27 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -1,9 +1,10 @@
--- Write your MySQL query statement below
+# Write your MySQL query statement below
 /*Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
 Return the result table in any order.
 The result format is in the following example.
 --*/
 SELECT activity_date as day, COUNT(distinct user_id) active_users 
 FROM Activity 
-WHERE activity_date BETWEEN  DATE_SUB('2019-07-27', INTERVAL 30 DAY) AND '2019-07-27' 
+WHERE activity_date BETWEEN  DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27' 
 GROUP BY  activity_date
+ORDER BY day
