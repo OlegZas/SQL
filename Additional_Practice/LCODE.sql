7899e3e1a07bfa3e53c708ddfa434b3d20305897
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-18 20:48:45 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -8,3 +8,17 @@ FROM Activity
 WHERE activity_date BETWEEN  DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27' 
 GROUP BY  activity_date
 ORDER BY day
+
+-- 2.
+/*
+Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
+Return the result table in any order.
+The result format is in the following example. */ 
+
+select t1.name 
+from SalesPerson t1
+where t1.sales_id not in (
+    select t3.sales_id 
+    from Company t2 join Orders t3
+    on t2.com_id = t3.com_id and t2.name = "RED"
+)
