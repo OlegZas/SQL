3c8d99f2f680197191f8b17bdb39e1c3300c3d95
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-19 18:20:46 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -36,3 +36,13 @@ The result format is in the following example.
  WHERE YEAR(time_stamp) = 2020
  GROUP BY user_id
  ;
+--4. 
+/*
+Write a solution to calculate the total time in minutes spent by each employee on each day at the office. Note that within one day, an employee can enter and leave more than once. The time spent in the office for a single entry is out_time - in_time.
+Return the result table in any order.
+The result format is in the following example.
+*/
+
+SELECT event_day as day, emp_id , SUM(out_time - in_time) as total_time
+FROM Employees 
+GROUP BY emp_id, event_day 
