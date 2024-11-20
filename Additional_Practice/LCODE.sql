127c43e32d024d3e0ee2411ddd08bdb7df0eac85
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-19 18:14:56 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -22,3 +22,17 @@ where t1.sales_id not in (
     from Company t2 join Orders t3
     on t2.com_id = t3.com_id and t2.name = "RED"
 )
+
+-- 11/19/24
+--3. 
+/*
+Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.
+Return the result table in any order.
+The result format is in the following example.
+*/
+ 
+ SELECT user_id, MAX(time_stamp) AS last_stamp          
+ FROM Logins 
+ WHERE YEAR(time_stamp) = 2020
+ GROUP BY user_id
+ ;
