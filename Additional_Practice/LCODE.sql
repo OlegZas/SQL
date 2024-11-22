a59e3e9e0ea5fe53dce85cfd38bbfeced06abfb0
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2024-11-22 12:11:56 -0800

    Update LCODE.sql

--- Additional_Practice/LCODE.sql
+++ Additional_Practice/LCODE.sql
@@ -114,3 +114,13 @@ SELECT *
 FROM Cinema 
 WHERE description <> 'boring' AND id%2 != 0
 ORDER BY rating desc 
+
+-- 11/22/24
+--11. 
+/*For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.
+Return the result table in any order.
+The result format is in the following example.*/
+
+SELECT date_id, make_name , COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners 
+FROM DailySales
+GROUP BY date_id, make_name;
