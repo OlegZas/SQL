19177fb546373de86f893daa93e5e5e3869f69ad
Oleg Zasukha <115661636+OlegZas@users.noreply.github.com>
2025-09-17 22:08:50 -0700

    Update Exercises_sql.sql

--- Dbeaver_SQL/Exercises_sql.sql
+++ Dbeaver_SQL/Exercises_sql.sql
@@ -1,7 +1,7 @@
 
 -- 9/16/25
--- ✅ SQL Exercises — ozSQL_AWS_RDS
--- 🔹 Users, Orders & Products
+--  SQL Exercises — ozSQL_AWS_RDS
+--  Users, Orders & Products
 
 -- 1. List all users who have never placed an order.
 select *
@@ -33,3 +33,15 @@ SELECT P.product_id, U.user_id, U.name
 FROM ozSQL_AWS_RDS.users U
 INNER JOIN ozSQL_AWS_RDS.orders ORD ON U.user_id = ORD.user_id 
 INNER JOIN ozSQL_AWS_RDS.products P ON ORD.product_id = P.product_id AND P.product_id IN(1001, 1002)
+
+-- 9/17/25
+-- 5. List users who placed their first order within 2 days of signing up.
+SELECT U.user_id, U.signup_date, min(ORD.purchase_date )
+FROM ozSQL_AWS_RDS.users U
+INNER JOIN ozSQL_AWS_RDS.orders ORD ON U.user_id = ORD.user_id 
+GROUP BY U.USER_ID, U.signup_date 
+HAVING MIN(day(purchase_date)) - day(U.signup_date ) < 2;
+-- 6. Find users who signed up in the same country.
+SELECT SELECT DISTINCT U.name AS USER1, U.country, U.signup_date, U2.name AS USER2, U2.country, U2.signup_date
+FROM ozSQL_AWS_RDS.users U
+INNER JOIN ozSQL_AWS_RDS.users U2 ON U.user_id < U2.user_id AND U.country = U2.country  ; 
